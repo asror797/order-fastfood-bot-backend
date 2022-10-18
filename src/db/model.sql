b93be21392ee2f8f94d65c4c0a3e928f662ed961
asror797 <asror797shahobiddinov@gmail.com>
2022-10-18 23:16:47 +0500

    model

---
+++ src/db/model.sql
@@ -0,0 +1,28 @@
+
+
+
+CREATE table users (
+   id SERIAL PRIMARY KEY,
+   telegram_id INT NOT NULL,
+   fullname VARCHAR(128) NOT NULL,
+   phone_number VARCHAR(64) ,
+)
+
+
+models-
+   -id   
+   -name
+
+car-
+   -id
+   -title
+   -price
+   -year 
+   -author_phone_number 
+   -img []
+   -created
+
+
+
+
+
