0b83cb56cc9576f9ba75ca42764192385f665beb
asror797 <asror797shahobiddinov@gmail.com>
2022-10-20 20:52:40 +0500

    fkrn

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,9 +1,12 @@
-const express = require('express')
+const express = require('express');
+const MarksController = require('../controllers/marks.controller');
 const router = express.Router()
 const UsersController = require('./../controllers/users.controller')
 
 router   
       .get('/users',UsersController.GET)
+      .get('/marks',MarksController.GET)
       .post('/user',UsersController.INSERT)
+      .post('/mark',MarksController.INSERT)
 
 module.exports = router;
